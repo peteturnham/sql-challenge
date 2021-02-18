b4c4d0c86cd53bb085e6db67bccf7fb1ea89069a
Pete Turnham <peteturnham@gmail.com>
2021-02-18 11:16:04 -0500

    added queries to sql_hw.sql

--- sql_hw.sql
+++ sql_hw.sql
@@ -51,3 +51,24 @@ title VARCHAR(30)
 );
 --displaying data from staff_titles
 SELECT * FROM staff_title;
+
+
+--joining databases employee data and salaries and displaying query info
+SELECT employee_data.employee_number, employee_data.last_name, employee_data.first_name, employee_data.sex, salaries.salary
+FROM employee_data
+INNER JOIN salaries ON
+employee_data.employee_number=salaries.employee_number;
+
+-- query for employees hired in 1986
+SELECT first_name, last_name, hire_date FROM employee_data
+WHERE hire_date IN (1986);
+
+SELECT * FROM deparment_managers;
+
+--query for department of employee with name and department name
+SELECT employee_data.employee_number, employee_data.last_name, employee_data.first_name, 
+deparment_managers.employee_number, deparment_managers.department_number
+FROM employee_data
+RIGHT JOIN deparment_managers ON
+employee_data.employee_number=deparment_managers.employee_number;
+
